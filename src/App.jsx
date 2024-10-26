59df604e2530199361c6e9dee0e96d33fc73a998
Sanket Kedare <sanketkedare200@gmail.com>
2024-10-27 02:45:48 +0530

    Bug fixed 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import ToDo from "./Modules/To-Do_List/ToDo.jsx";
 import Comment from "./Modules/Comment/Comment.jsx";
 import Projects from "./Project/Projects.jsx";
 import ShootingGuns from "./Modules/Shooting_Guns/ShootingGuns.jsx";
-import DiwaliGifts from "./Modules/DiWaliGifts/DiwaliGifts.jsx";
 import TicTacToe from "./Modules/TicTacToe/TicTacToe.jsx";
+import DiwaliGifts from "./Modules/DiWaliGifts/DiwaliGifts.jsx";
 
 /**
  * App Component
